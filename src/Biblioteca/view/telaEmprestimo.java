98e6452078bb1cef8bb807322b8d45a5a4ed9712
Kaléu <kaleuvlb@gmail.com>
2025-07-18 11:25:28 -0300

    Estrutura inicial da tela de emprestimo

--- src/Biblioteca/view/telaEmprestimo.java
+++ src/Biblioteca/view/telaEmprestimo.java
@@ -1,4 +1,58 @@
 package Biblioteca.view;
 
-public class telaEmprestimo {
+import javax.swing.*;
+import java.awt.*;
+
+public class telaEmprestimo extends JFrame{
+    public telaEmprestimo(){
+        setTitle("Empréstimo");
+        setSize(500,400);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JPanel painel = new JPanel();
+        painel.setLayout(new GridLayout(7, 5, 10, 10));
+
+        JLabel lblID =  new JLabel("ID: ");
+        JTextField txtID = new JTextField();
+
+        JLabel lblObra = new JLabel("Obra: ");
+        JTextField txtObra = new JTextField();
+
+        JLabel lblUsuario = new JLabel("Usuário: ");
+        JTextField txtUsuario = new JTextField();
+
+        JLabel lblDataDoEmprestimo= new JLabel("Data do empréstimo: ");
+        JTextField txtDataDoEmprestimo = new JTextField();
+
+        JLabel lblDataDeDevolucao = new JLabel("Data de devolução: ");
+        JTextField txtDataDeDevolucao = new JTextField();
+
+        JLabel lblDevolvido= new JLabel("Devolvido: ");
+        JTextField txtDevolvido = new JTextField();
+
+        JButton btnSalvar = new JButton("Salvar");
+        JButton btnCancelar = new JButton("Cancelar");
+
+        painel.add(lblID);
+        painel.add(txtID);
+        painel.add(lblObra);
+        painel.add(txtObra);
+        painel.add(lblUsuario);
+        painel.add(txtUsuario);
+        painel.add(lblDataDoEmprestimo);
+        painel.add(txtDataDoEmprestimo);
+        painel.add(lblDataDeDevolucao);
+        painel.add(txtDataDeDevolucao);
+        painel.add(btnSalvar);
+        painel.add(btnCancelar);
+
+        add(painel);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new telaEmprestimo().setVisible(true);
+        });
+    }
 }
