3c95857e74c78b28413689d5d415e6ddc214177c
Kaléu <kaleuvlb@gmail.com>
2025-07-21 10:00:25 -0300

    Implementação do Cadastrar usuário no menu principal

--- src/Biblioteca/view/telaMenuPrincipal.java
+++ src/Biblioteca/view/telaMenuPrincipal.java
@@ -5,49 +5,82 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class telaMenuPrincipal extends JFrame {
+    private JButton btnCadastrarUsuario;
+    private JButton btnEditarUsuario;
+    private JButton btnExcluirUsuario;
+    private JButton btnCadastrarObra;
+    private JButton btnRealizarEmprestimo;
+    private JButton btnDevolucaoObra;
+    private JButton btnPagarMulta;
+    private JButton btnListarObras;
+    private JButton btnBuscarObraTitulo;
+    private JButton btnBuscarObraAutor;
+    private JButton btnBuscarObraTipo;
+    private JButton btnRelatorioEmprestimosMes;
+    private JButton btnRelatorioObrasMaisEmprestadas;
+    private JButton btnRelatorioUsuariosAtrasados;
+    private JButton btnLogout;
 
     public telaMenuPrincipal() {
         setTitle("Menu Principal");
-        setSize(400, 600);
+        setSize(600, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
         JPanel painel = new JPanel();
-        painel.setLayout(new GridLayout(16, 1, 10, 10));
+        painel.setLayout(new GridLayout(15, 1, 5, 5));
 
-        JButton btnCadastrarUsuario = new JButton("1. Cadastrar Usuário");
-        JButton btnEditarUsuario = new JButton("2. Editar Usuário");
-        JButton btnExcluirUsuario = new JButton("3. Excluir Usuário");
-        JButton btnCadastrarObra = new JButton("4. Cadastrar Obra");
-        JButton btnEmprestimo = new JButton("5. Realizar Empréstimo");
-        JButton btnDevolucao = new JButton("6. Devolução de Obra");
-        JButton btnPagarMulta = new JButton("7. Pagar Devolução Pendente");
-        JButton btnListarObras = new JButton("8. Listar Obras");
-        JButton btnBuscarTitulo = new JButton("9. Buscar Obra por Título");
-        JButton btnBuscarAutor = new JButton("10. Buscar Obra por Autor");
-        JButton btnBuscarTipo = new JButton("11. Buscar Obra por Tipo");
-        JButton btnRelatorioMes = new JButton("12. Relatório de Empréstimos do Mês");
-        JButton btnRelatorioObras = new JButton("13. Relatório Obras + Emprestadas");
-        JButton btnRelatorioUsuarios = new JButton("14. Relatório Usuários + Atrasos");
-        JButton btnLogout = new JButton("15. Logout");
+        btnCadastrarUsuario = new JButton("1. Cadastrar Usuário");
+        btnEditarUsuario = new JButton("2. Editar Usuário");
+        btnExcluirUsuario = new JButton("3. Excluir Usuário");
+        btnCadastrarObra = new JButton("4. Cadastrar Obra");
+        btnRealizarEmprestimo = new JButton("5. Realizar Empréstimo");
+        btnDevolucaoObra = new JButton("6. Devolução de Obra");
+        btnPagarMulta = new JButton("7. Pagar Devolução Pendente");
+        btnListarObras = new JButton("8. Listar Obras");
+        btnBuscarObraTitulo = new JButton("9. Buscar Obra por Título");
+        btnBuscarObraAutor = new JButton("10. Buscar Obra por Autor");
+        btnBuscarObraTipo = new JButton("11. Buscar Obra por Tipo");
+        btnRelatorioEmprestimosMes = new JButton("12. Relatório de Empréstimos do Mês");
+        btnRelatorioObrasMaisEmprestadas = new JButton("13. Relatório de Obras Mais Emprestadas");
+        btnRelatorioUsuariosAtrasados = new JButton("14. Relatório de Usuários Atrasados");
+        btnLogout = new JButton("15. Logout");
 
         painel.add(btnCadastrarUsuario);
         painel.add(btnEditarUsuario);
         painel.add(btnExcluirUsuario);
         painel.add(btnCadastrarObra);
-        painel.add(btnEmprestimo);
-        painel.add(btnDevolucao);
+        painel.add(btnRealizarEmprestimo);
+        painel.add(btnDevolucaoObra);
         painel.add(btnPagarMulta);
         painel.add(btnListarObras);
-        painel.add(btnBuscarTitulo);
-        painel.add(btnBuscarAutor);
-        painel.add(btnBuscarTipo);
-        painel.add(btnRelatorioMes);
-        painel.add(btnRelatorioObras);
-        painel.add(btnRelatorioUsuarios);
+        painel.add(btnBuscarObraTitulo);
+        painel.add(btnBuscarObraAutor);
+        painel.add(btnBuscarObraTipo);
+        painel.add(btnRelatorioEmprestimosMes);
+        painel.add(btnRelatorioObrasMaisEmprestadas);
+        painel.add(btnRelatorioUsuariosAtrasados);
         painel.add(btnLogout);
 
         add(painel);
+
+        btnCadastrarUsuario.addActionListener(e -> {
+            new telaCadastroUsuario().setVisible(true);
+        });
+        btnEditarUsuario.addActionListener(e -> System.out.println("Editar Usuário clicado"));
+        btnExcluirUsuario.addActionListener(e -> System.out.println("Excluir Usuário clicado"));
+        btnCadastrarObra.addActionListener(e -> System.out.println("Cadastrar Obra clicado"));
+        btnRealizarEmprestimo.addActionListener(e -> System.out.println("Realizar Empréstimo clicado"));
+        btnDevolucaoObra.addActionListener(e -> System.out.println("Devolução de Obra clicado"));
+        btnPagarMulta.addActionListener(e -> System.out.println("Pagar Devolução Pendente clicado"));
+        btnListarObras.addActionListener(e -> System.out.println("Listar Obras clicado"));
+        btnBuscarObraTitulo.addActionListener(e -> System.out.println("Buscar Obra por Título clicado"));
+        btnBuscarObraAutor.addActionListener(e -> System.out.println("Buscar Obra por Autor clicado"));
+        btnBuscarObraTipo.addActionListener(e -> System.out.println("Buscar Obra por Tipo clicado"));
+        btnRelatorioEmprestimosMes.addActionListener(e -> System.out.println("Relatório de Empréstimos do Mês clicado"));
+        btnRelatorioObrasMaisEmprestadas.addActionListener(e -> System.out.println("Relatório de Obras Mais Emprestadas clicado"));
+        btnRelatorioUsuariosAtrasados.addActionListener(e -> System.out.println("Relatório de Usuários Atrasados clicado"));
+        btnLogout.addActionListener(e -> System.out.println("Logout clicado"));
     }
 
     public static void main(String[] args) {
