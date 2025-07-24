b0ecb71f5a0f331602d8a4851126f9a4898b69e4
Kaléu <kaleuvlb@gmail.com>
2025-07-24 11:36:17 -0300

    campos organizados

--- src/Biblioteca/view/adm/telaEmprestimo.java
+++ src/Biblioteca/view/adm/telaEmprestimo.java
@@ -3,51 +3,44 @@ package Biblioteca.view.adm;
 import javax.swing.*;
 import java.awt.*;
 
-public class telaEmprestimo extends JFrame{
-    public telaEmprestimo(){
-        setTitle("Empréstimo");
-        setSize(500,400);
+public class telaEmprestimo extends JFrame {
+
+    public telaEmprestimo() {
+        setTitle("Empréstimo de Obra");
+        setSize(450, 300);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
 
         JPanel painel = new JPanel();
-        painel.setLayout(new GridLayout(7, 5, 10, 10));
+        painel.setLayout(new GridLayout(5, 2, 10, 10));
 
-        JLabel lblID =  new JLabel("ID: ");
-        JTextField txtID = new JTextField();
+        JLabel lblMatricula = new JLabel("Matrícula do Usuário:");
+        JTextField txtMatricula = new JTextField();
 
-        JLabel lblObra = new JLabel("Obra: ");
-        JTextField txtObra = new JTextField();
+        JLabel lblTipoObra = new JLabel("Tipo da Obra:");
+        JComboBox<String> comboTipoObra = new JComboBox<>();
+        comboTipoObra.addItem("Artigo");
+        comboTipoObra.addItem("Livro");
+        comboTipoObra.addItem("Revista");
 
-        JLabel lblUsuario = new JLabel("Usuário: ");
-        JTextField txtUsuario = new JTextField();
-
-        JLabel lblDataDoEmprestimo= new JLabel("Data do empréstimo: ");
-        JTextField txtDataDoEmprestimo = new JTextField();
-
-        JLabel lblDataDeDevolucao = new JLabel("Data de devolução: ");
-        JTextField txtDataDeDevolucao = new JTextField();
-
-        JLabel lblDevolvido= new JLabel("Devolvido: ");
-        JTextField txtDevolvido = new JTextField();
+        JLabel lblCodigoObra = new JLabel("Código da Obra:");
+        JTextField txtCodigoObra = new JTextField();
 
         JButton btnSalvar = new JButton("Salvar");
         JButton btnCancelar = new JButton("Cancelar");
 
-        painel.add(lblID);
-        painel.add(txtID);
-        painel.add(lblObra);
-        painel.add(txtObra);
-        painel.add(lblUsuario);
-        painel.add(txtUsuario);
-        painel.add(lblDataDoEmprestimo);
-        painel.add(txtDataDoEmprestimo);
-        painel.add(lblDataDeDevolucao);
-        painel.add(txtDataDeDevolucao);
+        painel.add(lblMatricula);
+        painel.add(txtMatricula);
+        painel.add(lblTipoObra);
+        painel.add(comboTipoObra);
+        painel.add(lblCodigoObra);
+        painel.add(txtCodigoObra);
         painel.add(btnSalvar);
         painel.add(btnCancelar);
 
         add(painel);
+
+        btnCancelar.addActionListener(e -> dispose());
     }
 
     public static void main(String[] args) {
