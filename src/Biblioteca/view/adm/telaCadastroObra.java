c04612df78df7545f82ec09f54866689516cf993
Kaléu <kaleuvlb@gmail.com>
2025-07-24 11:30:10 -0300

    campos organizados

--- src/Biblioteca/view/adm/telaCadastroObra.java
+++ src/Biblioteca/view/adm/telaCadastroObra.java
@@ -3,52 +3,60 @@ package Biblioteca.view.adm;
 import javax.swing.*;
 import java.awt.*;
 
-public class telaCadastroObra extends JFrame{
-    public telaCadastroObra(){
+import Biblioteca.dao.ObraDao;
+import Biblioteca.model.Obra;
+
+public class telaCadastroObra extends JFrame {
+
+    public telaCadastroObra() {
         setTitle("Cadastro de Obra");
-        setSize(500,400);
+        setSize(500, 400);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
 
         JPanel painel = new JPanel();
-        painel.setLayout(new GridLayout(7, 5, 10, 10));
+        painel.setLayout(new GridLayout(6, 2, 10, 10));
 
-        JLabel lblCodigo =  new JLabel("Código ");
-        JTextField txtCodigo = new JTextField();
-
-        JLabel lblTitulo = new JLabel("Titulo: ");
+        JLabel lblTitulo = new JLabel("Título:");
         JTextField txtTitulo = new JTextField();
 
-        JLabel lblAutor = new JLabel("Autor: ");
+        JLabel lblAutor = new JLabel("Autor:");
         JTextField txtAutor = new JTextField();
 
-        JLabel lblAnoPublicacao = new JLabel("Ano de Publicação: ");
+        JLabel lblTipoObra = new JLabel("Tipo da obra:");
+        JComboBox<String> comboTipoObra = new JComboBox<>();
+        comboTipoObra.addItem("Artigo");
+        comboTipoObra.addItem("Livro");
+        comboTipoObra.addItem("Revista");
+
+        JLabel lblAnoPublicacao = new JLabel("Ano de Publicação:");
         JTextField txtAnoPublicacao = new JTextField();
 
-        JLabel lblStatus = new JLabel("Status: ");
-        JTextField txtStatus = new JTextField();
+        JLabel lblCodigoObra = new JLabel("Código:");
+        JTextField txtCodigoObra = new JTextField();
 
         JButton btnSalvar = new JButton("Salvar");
         JButton btnCancelar = new JButton("Cancelar");
 
-        painel.add(lblCodigo);
-        painel.add(txtCodigo);
         painel.add(lblTitulo);
         painel.add(txtTitulo);
         painel.add(lblAutor);
         painel.add(txtAutor);
+        painel.add(comboTipoObra);
         painel.add(lblAnoPublicacao);
         painel.add(txtAnoPublicacao);
-        painel.add(lblStatus);
-        painel.add(txtStatus);
+        painel.add(lblCodigoObra);
+        painel.add(txtCodigoObra);
         painel.add(btnSalvar);
         painel.add(btnCancelar);
 
         add(painel);
+
+        btnCancelar.addActionListener(e -> dispose());
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() ->{
+        SwingUtilities.invokeLater(() -> {
             new telaCadastroObra().setVisible(true);
         });
     }
